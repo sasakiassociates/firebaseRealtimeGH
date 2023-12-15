28dd24334ee2c583dc45e187bc35b03207fb85ef
KaipoCraft <71410871+KaipoCraft@users.noreply.github.com>
2023-12-15 12:31:20 -0500

    renaming and folder restructure

---
+++ src/realtimeGHComponent/realtimeGHComponentInfo.cs
@@ -0,0 +1,26 @@
+﻿using Grasshopper;
+using Grasshopper.Kernel;
+using System;
+using System.Drawing;
+
+namespace firebaseRealtime
+{
+    public class realtimeGHComponentInfo : GH_AssemblyInfo
+    {
+        public override string Name => "firebaseRealtime";
+
+        //Return a 24x24 pixel bitmap to represent this GHA library.
+        public override Bitmap Icon => null;
+
+        //Return a short string describing the purpose of this GHA library.
+        public override string Description => "";
+
+        public override Guid Id => new Guid("7f2b7cfc-7089-4531-9232-12effc108504");
+
+        //Return a string identifying you or your company.
+        public override string AuthorName => "";
+
+        //Return a string representing your preferred contact details.
+        public override string AuthorContact => "";
+    }
+}
